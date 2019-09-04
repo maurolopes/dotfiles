ed65934be389c08362007e62081d836010ef19bc
Mauro Lopes <mauro@tenx.tech>
2019-09-04 18:15:53 +0800

    Remove shackle

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -84,29 +84,6 @@
   :diminish nil
   :config (which-key-mode +1))
 
-;; (use-package shackle ; tame window popups
-;;   :defer nil
-;;   :config
-;;   (setq shackle-rules
-;;         '((compilation-mode              :select nil                                               )
-;;           ("*undo-tree*"                                                    :size 0.25 :align right)
-;;           ("*eshell*"                    :select t                          :other t               )
-;;           ("*Shell Command Output*"      :select nil                                               )
-;;           ("\\*Async Shell.*\\*" :regexp t :ignore t                                               )
-;;           (occur-mode                    :select nil                                   :align t    )
-;;           ("*Help*"                      :select t   :inhibit-window-quit t   :other t             )
-;;           ("*Completions*"                                                  :size 0.3  :align t    )
-;;           ("*Messages*"                    :select nil :inhibit-window-quit t :other t             )
-;;           ("\\*[Wo]*Man.*\\*"    :regexp t   :select t :inhibit-window-quit t :other t             )
-;;           ("\\*poporg.*\\*"      :regexp t :select t                        :other t               )
-;;           ("\\`\\*helm.*?\\*\\'" :regexp t                                  :size 0.3  :align t    )
-;;           ("*Calendar*"                  :select t                          :size 0.3  :align below)
-;;           ("*info*"                      :select t   :inhibit-window-quit t               :same t)
-;;           (magit-status-mode             :select t   :inhibit-window-quit t               :same t)
-;;           (magit-log-mode                :select t   :inhibit-window-quit t               :same t)
-;;           (:select t)))
-;;   (shackle-mode +1))
-
 (use-package undo-tree
   :diminish nil
   :defer nil
