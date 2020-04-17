d8818e81708ebf90a5321ab09f14a08a7db745dc
Mauro Lopes <maurolopes@gmail.com>
2020-04-18 00:42:09 +0800

    Installer

---
+++ install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+sudo ln -s "$(realpath 11-usb-keyboard.conf)" /etc/X11/xorg.conf.d/11-usb-keyboard.conf
+sudo ln -s "$(realpath 70-synaptics.conf)" /etc/X11/xorg.conf.d/70-synaptics.conf
+ln -s "$(realpath dircolors)" ~/.dircolors
+mkdir -p ~/.emacs.d && ln -s "$(realpath .emacs.d/custom.el)" ~/.emacs.d/custom.el
+mkdir -p ~/.emacs.d && ln -s "$(realpath .emacs.d/init.el)" ~/.emacs.d/init.el
+ln -s "$(realpath gitconfig)" ~/.gitconfig
+ln -s "$(realpath gitignore)" ~/.gitignore
+ln -s "$(realpath i3)" ~/.config/i3/config
+ln -s "$(realpath i3status-rust)" ~/.config/i3/status.toml
+ln -s "$(realpath profile)" ~/.profile
+mkdir -p ~/.lein && ln -s "$(realpath profiles.clj)" ~/.lein/profiles.clj
+ln -s "$(realpath xprofile)" ~/.xprofile
+ln -s "$(realpath Xmodmap)" ~/.Xmodmap
