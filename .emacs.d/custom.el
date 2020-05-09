65dd158f187493855f375d49368e9fa82936bc6c
Mauro Lopes <maurolopes@gmail.com>
2020-05-09 15:53:26 +0800

    Emacs: use Hack font

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -152,5 +152,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:height 100 :family "Hack"))))
  '(avy-lead-face ((t (:foreground "orange"))))
  '(avy-lead-face-0 ((t (:foreground "white")))))
