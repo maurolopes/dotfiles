0f9f5f0f3a4dff2277c25b64d0dc8be164489735
Mauro Lopes <maurolopes@gmail.com>
2020-05-20 00:10:57 +0800

    Initial adaptation to make init.el work with Emacs 27

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,8 +25,9 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-(package-initialize)
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)
+  (package-initialize))
 
 (unless package-archive-contents
   (package-refresh-contents)
