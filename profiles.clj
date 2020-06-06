6972ede5a378810121d04bf83228788944564aa7
Mauro Lopes <maurolopes@gmail.com>
2020-06-06 14:47:57 +0800

    Update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,6 @@
 {:user
- {:plugins [[refactor-nrepl "2.5.0-SNAPSHOT"]
-            [cider/cider-nrepl "0.22.4"]
+ {:plugins [[refactor-nrepl "2.5.0"]
+            [cider/cider-nrepl "0.25.0-alpha1"]
             [lein-auto "0.1.3"]]
   :dependencies [[profit "0.1.0-SNAPSHOT"]]
   :injections   [(require '$)
